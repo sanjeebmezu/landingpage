1d3b350e992af3d11dbf73f3c4ccdd13696cfb35
sanjeebmezu@gmail.com <sanjeevmeju@gmail.com>
2026-09-12 12:33:44 +0545

    Update Meta Pixel ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '2622774838177398');
+fbq('init', '2097480527829616');
 fbq('track', 'PageView');
 `
           }}
@@ -62,7 +62,7 @@ fbq('track', 'PageView');
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=2622774838177398&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=2097480527829616&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
